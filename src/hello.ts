6e0f78b35861fc1074478496a63acbd8d44ddb54
cloudliner <cloudliner.jp@gmail.com>
2018-07-26 10:30:55 +0900

    change for declare

--- src/hello.ts
+++ src/hello.ts
@@ -1,6 +1,14 @@
+const a = 1;
+
 function hello() {
   const name = 'Steve';
   // console.log(`Hello, ${name}`);
-  Logger.log(`Hello, ${name}`);
+  Logger.log(`Hello, ${name}, ${a}, ${b}, ${c}`);
   test();
 }
+
+function newTest() {
+  Logger.log(`newTest ${a}, ${b}, ${c}`);
+}
+
+const b = 2;
--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,7 @@
+const c = 3;
+
 function test() {
   // console.log('Test');
-  Logger.log('Test');
+  Logger.log(`Test ${a}, ${b}, ${c}`);
+  newTest();
 }
