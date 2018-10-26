f4c701e57452571c194f03f7d798565cf812a298
cloudliner <cloudliner.jp@gmail.com>
2018-10-26 13:03:12 +0900

    minor changes

--- src/hello.ts
+++ src/hello.ts
@@ -3,12 +3,12 @@ const a = 1;
 function hello() {
   const name = 'Steve';
   // console.log(`Hello, ${name}`);
-  Logger.log(`Hello, ${name}, ${a}, ${b}, ${c}`);
+  Logger.log(`Hello: ${name}, ${a}, ${b}, ${c}`);
   test();
 }
 
 const newTest = () => {
-  Logger.log(`newTest ${a}, ${b}, ${c}`);
+  Logger.log(`newTest: ${a}, ${b}, ${c}`);
   const email = Session.getActiveUser().getEmail();
   const body = `Test, ${a}, ${b}, ${c}`;
   MailApp.sendEmail(email, 'GAS-Log: Test', body,
--- src/test.ts
+++ src/test.ts
@@ -2,6 +2,6 @@ const c = 3;
 
 function test() {
   // console.log('Test');
-  Logger.log(`Test ${a}, ${b}, ${c}`);
+  Logger.log(`Test: ${a}, ${b}, ${c}`);
   newTest();
 }
